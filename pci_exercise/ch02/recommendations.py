729c445701a721fee2c3d954a7f383494ca6f804
ksomemo <kntsmy@hotmail.co.jp>
2015-09-22 04:03:27 +0900

    ピアソン相関に依るスコアを実装した

--- pci_exercise/ch02/recommendations.py
+++ pci_exercise/ch02/recommendations.py
@@ -71,4 +71,36 @@ def sim_distance(prefs, person1, person2):
     sum_of_squares = sum([pow(prefs[person1][item] - prefs[person2][item], 2)
                           for item in prefs[person1] if item in prefs[person2]])
 
-    return 1 / (1 + sum_of_squares)
+    return 1 / (1 + sqrt(sum_of_squares))
+
+
+def sim_pearson(prefs, p1, p2):
+    """Returns the Pearson correlation coefficient for p1 and p2
+    Get the list of mutually rated items"""
+    si = {}
+    for item in prefs[p1]:
+        if item in prefs[p2]:
+            si[item] = 1
+
+    n = len(si)
+    if n == 0:
+        return 0
+
+    # Sums of all the preferences
+    sum1 = sum([prefs[p1][it] for it in si])
+    sum2 = sum([prefs[p2][it] for it in si])
+
+    # Sums of the squares
+    sum1_sq = sum([pow(prefs[p1][it], 2) for it in si])
+    sum2_sq = sum([pow(prefs[p2][it], 2) for it in si])
+
+    # Sum of the products
+    p_sum = sum([prefs[p1][it] * prefs[p2][it] for it in si])
+
+    # Calculate r (Pearson score)
+    num = p_sum - (sum1 * sum2 / n)
+    den = sqrt((sum1_sq - pow(sum1, 2) / n) * (sum2_sq - pow(sum2, 2) / n))
+    if den == 0:
+        return 0
+
+    return num / den
