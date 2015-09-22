7dc1c3700abadd695562fa82765b339319c04e87
ksomemo <kntsmy@hotmail.co.jp>
2015-09-22 16:02:41 +0900

    アイテム推薦を実装した

--- pci_exercise/ch02/recommendations.py
+++ pci_exercise/ch02/recommendations.py
@@ -114,3 +114,34 @@ def top_matches(prefs, person, n=5, similarity=sim_pearson):
     scores.sort()
     scores.reverse()
     return scores[0:n]
+
+
+def get_recommendations(prefs, person, similarity=sim_pearson):
+    """Gets recommendations for a person by using a weighted average
+    of every other user's rankings"""
+    totals = {}
+    sim_sums = {}
+    for other in prefs:
+        if other == person:
+            continue
+        sim = similarity(prefs, person, other)
+
+        if sim <= 0:
+            continue
+        for item in prefs[other]:
+            # only score movies I haven't seen yet
+            if item not in prefs[person] or prefs[person][item] == 0:
+                # Similarity * Score
+                totals.setdefault(item, 0)
+                totals[item] += prefs[other][item] * sim
+                # Sum of similarities
+                sim_sums.setdefault(item, 0)
+                sim_sums[item] += sim
+
+    # Create the normalized list
+    rankings = [(total / sim_sums[item], item)
+                for item, total in totals.items()]
+
+    rankings.sort()
+    rankings.reverse()
+    return rankings
